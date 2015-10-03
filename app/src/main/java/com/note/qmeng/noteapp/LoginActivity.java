86bcb886e424cbe3433b7ed45a8284bcc35d3cb1
Qnail <qmeng2014@gmail.com>
2015-10-03 11:24:14 +0800

    add auto login and remember password. add logo page--logging. fix bug login success press back button would back to login page.

--- app/src/main/java/com/note/qmeng/noteapp/LoginActivity.java
+++ app/src/main/java/com/note/qmeng/noteapp/LoginActivity.java
@@ -1,53 +1,129 @@
 package com.note.qmeng.noteapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
-public class LoginActivity extends Activity implements OnClickListener{
-    private EditText accountEditText = null;
-    private EditText pwdEditText = null;
+public class LoginActivity extends Activity{
+    /**
+     *  base information in login page.
+     */
+    private EditText userName,password;
+    private String userNameStr,passwordStr;
+    private CheckBox remPwd,autoLogin;
+    private Button loginButton;
+    private SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
-
-        //set up click listeners for all the buttons.
-
-        View loginButton = this.findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(this);
-    }
-
-    public void onClick(View v)
-    {
-        switch (v.getId())
+        //get user instance
+        sp = this.getSharedPreferences("userInfo", Context.MODE_WORLD_READABLE);
+        userName = (EditText)findViewById(R.id.accountEditText);
+        password = (EditText)findViewById(R.id.pwdEditText);
+        remPwd = (CheckBox)findViewById(R.id.cbRemPwd);
+        autoLogin = (CheckBox)findViewById(R.id.cbAutoLogin);
+        loginButton = (Button)findViewById(R.id.loginButton);
+        //check CheckBox
+        if(sp.getBoolean("IS_CHECKED",false))
         {
-            case R.id.loginButton:
-                if(checkPassWord())
-                {
-                    Intent i = new Intent(this,IndexActivity.class);
-                    startActivity(i);
-                }else {
-                    //TODO the password is wrong do sth.
-                }
-                break;
+            remPwd.setChecked(true);
+            userName.setText(sp.getString("USER_NAME",""));
+            password.setText(sp.getString("PASSWORD",""));
+
+            //check autoLogin
+            if(sp.getBoolean("AUTO_LOGIN",false))
+            {
+                autoLogin.setChecked(true);
+                Intent intent = new Intent(this,LogoActivity.class);
+                startActivity(intent);
+                this.finish();
+            }
         }
+
+        loginButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userNameStr = userName.getText().toString();
+                passwordStr = password.getText().toString();
+                if(checkInfo(userNameStr,passwordStr))
+                {
+                    Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+                    if(remPwd.isChecked())
+                    {
+                        SharedPreferences.Editor editor= sp.edit();
+                        editor.putString("USER_NAME",userNameStr);
+                        editor.putString("PASSWORD",passwordStr);
+                        editor.commit();
+                    }
+                    Intent intent = new Intent(LoginActivity.this,LogoActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else
+                {
+                    Toast.makeText(LoginActivity.this,"用户名或密码错误，请重新登录！",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        remPwd.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(remPwd.isChecked())
+                {
+                    sp.edit().putBoolean("IS_CHECKED",true).commit();
+                }else
+                {
+                    sp.edit().putBoolean("IS_CHECKED",false).commit();
+                }
+            }
+        });
+
+        autoLogin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(autoLogin.isChecked())
+                {
+                    sp.edit().putBoolean("AUTO_LOGIN",true).commit();
+                }else
+                {
+                    sp.edit().putBoolean("AUTO_LOGIN",false).commit();
+                }
+            }
+        });
     }
 
-    public boolean checkPassWord(){
+//    public void onClick(View v)
+//    {
+//        switch (v.getId())
+//        {
+//            case R.id.loginButton:
+//                if(checkPassWord())
+//                {
+//                    Intent i = new Intent(this,LogoActivity.class);
+//                    startActivity(i);
+//                }else {
+//                    //the password is wrong do sth.
+//                }
+//                break;
+//        }
+//    }
+
+    public boolean checkInfo(String userName,String password){
         boolean result = false;
-        accountEditText = (EditText)findViewById(R.id.accountEditText);
-        pwdEditText = (EditText)findViewById(R.id.pwdEditText);
-        if(accountEditText.getText().toString().equals("seu")&&pwdEditText.getText().toString().equals("123"))
+        if(userName.equals("seu")&&password.equals("123"))
         {
             result = true;
         }
-
         return result;
     }
 }
---
+++ app/src/main/java/com/note/qmeng/noteapp/LogoActivity.java
@@ -0,0 +1,33 @@
+package com.note.qmeng.noteapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+/**
+ * Created by MQ on 2015/10/3.
+ */
+public class LogoActivity  extends Activity{
+    private ProgressBar progressBar;
+    private Button backButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logo);
+        progressBar = (ProgressBar)findViewById(R.id.pgBar);
+        backButton = (Button)findViewById(R.id.backButton);
+
+        Intent intent = new Intent(this,IndexActivity.class);
+        startActivity(intent);
+        finish();
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
